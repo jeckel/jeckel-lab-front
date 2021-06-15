d87c76d4de2c55f265ebdcb4faca23ccd188b4ba
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2021-06-15 17:01:25 +0200

    Refactor contact form

--- src/components/organisms/Form.ContactForm.vue
+++ src/components/organisms/Form.ContactForm.vue
@@ -1,11 +1,11 @@
 <template>
-  <FlashMessage :message="completeMessage" :success="completeSuccess" v-if="isCompleted"/>
+  <FlashMessage :message="completeMessage" :success="completeSuccess" v-if="completed"/>
   <div class="form">
-  <form v-on:submit="submitForm">
+  <form @submit.prevent="submitForm">
     <FormFormRawTextField
       id="contact_from_name"
       required
-      :disabled="isDisabled"
+      :disabled="disabled"
       label="Votre nom"
       v-model="contactMessage.fromName"
     />
@@ -13,14 +13,14 @@
       type="email"
       id="contact_from_email"
       required
-      :disabled="isDisabled"
+      :disabled="disabled"
       label="Votre E-Mail"
       v-model="contactMessage.fromEmail"
     />
     <FormFormRawTextField
       id="contact_subject"
       required
-      :disabled="isDisabled"
+      :disabled="disabled"
       label="Sujet"
       v-model="contactMessage.subject"
     />
@@ -28,82 +28,66 @@
       id="contact_message"
       type="textarea"
       required
-      :disabled="isDisabled"
+      :disabled="disabled"
       label="Message"
       v-model="contactMessage.content"
       />
-    <FormFormRawSubmit label="Envoyer" icon="fas fa-paper-plane" v-if="isEdit"/>
-    <div class="loader" v-if="isLoading">
+    <FormFormRawSubmit label="Envoyer" icon="fas fa-paper-plane" v-if="! disabled"/>
+    <div class="loader" v-if="loading">
       <Loader/>
     </div>
   </form>
+<!--    <div>{{ contactMessage }}</div>-->
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { reactive, toRefs } from 'vue';
 import { ContactMessage } from '@/api';
 import FormFormRawTextField from '@/components/molecules/Form.FormRaw.TextField.vue';
 import FormFormRawSubmit from '@/components/molecules/Form.FormRaw.Submit.vue';
 import Loader from '@/components/atoms/Loader.vue';
 import FlashMessage from '@/components/atoms/FlashMessage.vue';
 import { ApiResponse } from '@/models/contact.interface';
+import { ContactFormMessageType } from '@/models/ContactFormTypes.interface';
 
-@Options({
+export default {
   components: {
     FormFormRawTextField, FormFormRawSubmit, Loader, FlashMessage,
   },
-})
-export default class FormContactForm extends Vue {
-  contactMessage = {
-    fromName: '', fromEmail: '', subject: '', content: '',
-  }
 
-  edit = true
+  // eslint-disable-next-line class-methods-use-this
+  setup(): Record<string, unknown> {
+    const state = reactive({
+      contactMessage: {} as ContactFormMessageType,
+      loading: false,
+      disabled: false,
+      completed: false,
+      completeMessage: 'Votre message',
+      completeSuccess: true,
+    });
 
-  loading = false
+    function submitForm() {
+      state.loading = true;
+      state.disabled = true;
 
-  completed = false
+      ContactMessage.sendContactMessage(state.contactMessage)
+        .then((response: ApiResponse) => {
+          state.completed = true;
+          state.loading = false;
+          state.completeMessage = response.message;
+          state.completeSuccess = response.success;
+        })
+        .catch(() => {
+          state.completed = true;
+          state.completeMessage = 'Error';
+          state.completeSuccess = false;
+        });
+    }
 
-  completeMessage = 'Votre message a bien été envoyé.'
-
-  completeSuccess = true
-
-  output = ''
-
-  get isDisabled(): boolean {
-    return !this.isEdit;
-  }
-
-  get isEdit(): boolean {
-    return this.edit && !this.loading && !this.completed;
-  }
-
-  get isLoading(): boolean {
-    return this.loading;
-  }
-
-  get isCompleted(): boolean {
-    return this.completed;
-  }
-
-  submitForm(e: Event): void {
-    this.loading = true;
-    e.preventDefault();
-
-    ContactMessage.sendContactMessage(this.contactMessage)
-      .then((response: ApiResponse) => {
-        this.completed = true;
-        this.loading = false;
-        this.completeMessage = response.message;
-        this.completeSuccess = response.success;
-      })
-      .catch(() => {
-        this.completed = true;
-        this.completeMessage = 'Error';
-      });
-  }
-}
+    return { ...toRefs(state), submitForm };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
